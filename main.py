53705a178fbe10e6659344ec7b6c811016136771
zulu-05 <zulumginqi@gmail.com>
2024-11-17 14:52:44 +0000

    added main application file

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from MainWindow import MainWindow
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == ""__main__":
+    main()
